bdedd445d6fb33dc80a247c75de224b033b5a1d0
Riccardo Carotenuto <riccardo.carotenuto@sdggroup.com>
2026-07-15 10:21:07 +0200

    with this macro we create the tables in our dev schema if we're operating in dev environment. Otherwise, the tables will be created within the schema specified in the project.yml file

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,28 +1,17 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-    {# Prende lo schema di default del target nel profilo dbt #}
-    {% set default_schema = target.schema %}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
     
-    {%- if custom_schema_name is none %}
-        {%- set schema_name = default_schema %}
-    {%- else %}
-        {%- set schema_name = custom_schema_name | trim %}
-    {%- endif %}
-
-    {%- if execute -%}
-        {%- set query_schema_check %}
-            select 1
-            from {{ node.database }}.information_schema.schemata
-            where lower(schema_name) = lower('{{ schema_name }}')
-        {%- endset -%}
-
-        {%- set results = run_query(query_schema_check) -%}
-        {{ log("Righe trovate: " ~ results.rows | length, info=True) }}
-        {%- if results.rows | length == 0 -%}
-            {%- set err_msg = "The model '" ~ node.name ~ "' needs the schema '" ~ schema_name ~ "', to be created in the catalog'" ~ node.database ~ "'" -%}
-            {{ exceptions.raise_compiler_error(err_msg) }}
+    {# Se siamo in PROD o TEST, usiamo lo schema del modello (es. bronze, silver, gold) #}
+    {%- if target.name in ['prd', 'tst'] -%}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ custom_schema_name | trim }}
         {%- endif -%}
+
+    {%- else -%}
+        {{ default_schema }}
     {%- endif -%}
 
-    {{ schema_name }}
-
-{% endmacro %}
+{%- endmacro %}
